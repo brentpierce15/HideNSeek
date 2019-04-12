151035941b6969ddc7817a769c4501e8d5c65f74
brentpierce15 <brentpierce15@augustana.edu>
2019-04-11 20:58:15 -0500

    -Minor layout changes

--- app/src/main/java/com/example/hide_n_seek/EndScreen.java
+++ app/src/main/java/com/example/hide_n_seek/EndScreen.java
@@ -1,6 +1,7 @@
 package com.example.hide_n_seek;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,13 +9,19 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class EndScreen extends AppCompatActivity {
 
     private TextView textView;
+    private ImageView img;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -44,6 +51,29 @@ public class EndScreen extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         textView.setText(winner + " Found the Hider!");
         textView.setTextSize(20);
+
+        img = (ImageView) findViewById(R.id.img);
+        getPicture();
+    }
+
+    private void getPicture(){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference homeScreenRef = storageRef.child("images/ending.jpg");
+        final long FIVE_MEGABYTES = 1024 * 1024 * 5;
+        homeScreenRef.getBytes(FIVE_MEGABYTES).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Log.d("success","got picture");
+                img.setImageBitmap(BitmapFactory.decodeByteArray(bytes,0,bytes.length));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
     }
 
 }
